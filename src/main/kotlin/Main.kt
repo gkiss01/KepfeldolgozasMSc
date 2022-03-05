f2970af5f8d6e6a4b23e7b268923e98b9f4c673b
gkiss01 <gergokiss05@gmail.com>
2022-03-05 13:24:05 +0100

    Téglalapok meghatározása a felosztások száma alapján

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
 import nu.pattern.OpenCV
-import org.opencv.core.Core
-import org.opencv.core.Mat
-import org.opencv.core.Scalar
-import org.opencv.core.Size
+import org.opencv.core.*
 import org.opencv.highgui.HighGui
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
@@ -14,7 +11,7 @@ const val WINDOW_NAME_PROCESSED = "Hand detection (processed)"
 
 fun main(args: Array<String>) {
     OpenCV.loadLocally()
-    
+
     val dimension = Toolkit.getDefaultToolkit().screenSize
 
     HighGui.namedWindow(WINDOW_NAME_ORIGINAL, HighGui.WINDOW_NORMAL)
@@ -32,10 +29,12 @@ fun main(args: Array<String>) {
         HighGui.imshow(WINDOW_NAME_ORIGINAL, img)
 
         val processedImg = img.clone()
-        keepHand(processedImg)
-        HighGui.imshow(WINDOW_NAME_PROCESSED, processedImg)
+        //keepHand(processedImg)
+        //HighGui.imshow(WINDOW_NAME_PROCESSED, processedImg)
 
-        HighGui.waitKey()
+        //HighGui.waitKey()
+        
+        generatePartMasks(processedImg, 3)
     }
 
     HighGui.waitKey()
@@ -66,6 +65,35 @@ fun keepHand(src: Mat) {
     Imgproc.dilate(src, src, kernel)
 }
 
+fun generatePartMasks(src: Mat, parts: Int): List<Mat> {
+    val masks = mutableListOf<Mat>()
+    val rects = generatePartRects(src, parts)
+
+    for (i in 0 until parts) {
+        //Imgproc.drawContours(src, listOf(rects[i].toMatOfPoints()), -1, Scalar(0.0, 0.0, 0.0), 2)
+        Imgproc.rectangle(src, rects[i], Scalar(0.0, 0.0, 0.0), -1)
+        //Imgproc.fillPoly(src, listOf(rect.toMatOfPoints()), Scalar(0.0, 0.0, 0.0))
+
+        HighGui.imshow(WINDOW_NAME_PROCESSED, src)
+        HighGui.waitKey()
+    }
+
+    return masks
+}
+
+fun generatePartRects(src: Mat, parts: Int): List<Rect> {
+    val rects = mutableListOf<Rect>()
+    val intervals = splitInterval(0, src.cols() - 1, parts)
+
+    for (i in 0 until parts) {
+        val point1 = Point(intervals[i].first.toDouble(), 0.0)
+        val point2 = Point(intervals[i].second.toDouble(), src.height().toDouble() - 1.0)
+        rects.add(Rect(point1, point2))
+    }
+
+    return rects
+}
+
 fun splitInterval(start: Int, end: Int, parts: Int): List<Pair<Int, Int>> {
     val size = floor((end - start + 1) / parts.toDouble())
     var mod = (end - start + 1) % parts.toDouble()
@@ -82,4 +110,11 @@ fun splitInterval(start: Int, end: Int, parts: Int): List<Pair<Int, Int>> {
     }
 
     return intervals
+}
+
+fun Rect.toMatOfPoints(): MatOfPoint {
+    val tr = Point((x + width).toDouble(), y.toDouble())
+    val bl = Point(x.toDouble(), (y + height).toDouble())
+
+    return MatOfPoint(tl(), tr, br(), bl)
 }
